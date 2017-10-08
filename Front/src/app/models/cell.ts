3ac2d7f843d65fa2324139b683131fa8a2456f5c
Agustin Prado <aguprado@gmail.com>
2017-10-08 16:03:56 -0300

    Added methods to models, created grid

--- Front/src/app/models/cell.ts
+++ Front/src/app/models/cell.ts
@@ -18,5 +18,53 @@ export class Cell {
         this.minedNeighbors = data.minedNeighbors || 0;
         this.status = data.status;
     }
+    
+    //sets cell color according with minedNeighbors quantity 
+    setColor = () => { 
+        switch (this.minedNeighbors) {
+            case 1:
+              this.color = 'blue';
+              break;
+            case 2:
+              this.color =  'green';
+              break;
+            case 3:
+              this.color =  'red';
+              break;
+            case 4:
+              this.color =  'violet';
+              break;
+            case 5:
+              this.color =  'orange';
+              break;
+            case 6:
+              this.color =  'yellow';
+              break;
+            case 7:
+              this.color =  'red';
+              break;
+            case 8:
+                this.color =  'red';
+                break;
+            default:
+                this.color =  '#333';
+                break;
+          }    
+    };
 
+    //returns true if cell is neighbor of the current cell (this)
+    //this can be done in a single line with && but is more readable this way
+    isNeighbor = (cell:Cell) => {
+        /* Neighbors:
+                    (x-1,y-1) - (x,y-1) - (x+1,y-1)
+                    (x-1,y)   - (x,y)   - (x+1,y)
+                    (x-1,y+1) - (x,y+1) - (x+1,y+1)
+        */
+        if (cell.row == this.row) {
+            return (cell.column == this.column+1 ||  cell.column == this.column-1);
+        }
+        if (cell.row+1 == this.row || cell.row-1 == this.row) {
+            return (cell.column == this.column || cell.column == this.column+1 || cell.column == this.column-1);
+        }
+    }
 }
--- Front/src/app/models/grid.ts
+++ Front/src/app/models/grid.ts
@@ -11,6 +11,52 @@ export class Grid {
         this.rows = data.rows;
         this.columns = data.columns;
         this.cells = data.cells || [];
+        this.setCells();
+    }
+
+    //creates cells and sets mines
+    private setCells = () => {
+        if (this.cells.length) {
+            //if cells passed by parameter in a loaded game, just create the cells objects from the input cells
+            this.cells = this.cells.map(cell => {
+                return new Cell(cell);
+            });
+        } else {
+            let toMine = this.setMines();
+            for (let row = 0; row < this.rows; row++) {
+                for (let column = 0; column < this.rows; column++) {
+                    toMine.find(x => {return x.row == row && x.column == column}) ? this.cells.push(new Cell({row: row, column: column, mined: true})) : this.cells.push(new Cell({row: row, column: column, mined: false}));
+                }
+            }
+            //set the quantity of mines neighbors for each cell in the grid
+            this.setNeighbors();
+        }
+    }
+
+    //place the <this.mines> mines into random cells
+    private setMines = () => {
+        let toMine = [];
+        while (toMine.length != this.mines) {
+            let row = Math.floor(Math.random() * this.rows);
+            let column =  Math.floor(Math.random() * this.columns);
+            if (!toMine.find(x => {return x.row == row && x.col == column})) {
+                toMine.push({row: row, column:column});
+            }
+        }
+        return toMine;
+    }
+
+    //set the quantity of mines neighbors for each cell in the grid
+    private setNeighbors = () => {
+        let mined = this.cells.filter(x => {return x.mined});
+        this.cells.filter(x => {return !x.mined}).map(cell => {
+            mined.forEach(mined => { 
+                if (cell.isNeighbor(mined)) { 
+                    cell.minedNeighbors += 1;
+                    cell.setColor();
+                }
+            });
+        });
     }
 
 }
